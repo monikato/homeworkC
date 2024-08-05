7b367e9685e57f2916a6982b62a83dc8951fff2a
monikato <katosilman@gmail.com>
2024-08-05 18:35:14 +0300

    Add files via upload

---
+++ main26.05.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+void filterText(string &text, bool Latin, bool Cyrillic, bool Punctuation, bool Digits)
+{
+    for (size_t i = 0; i < text.length(); i++)
+    {
+        if ((Latin && isalpha(text[i]) && text[i] < 128) ||
+            (Cyrillic && isalpha(text[i]) && text[i] >= 128) ||
+            (Punctuation && ispunct(text[i])) ||
+            (Digits && isdigit(text[i])))
+        {
+            text[i] = ' ';
+        }
+    }
+}
+
+void chooseFilters(bool &Latin, bool &Cyrillic, bool &Punctuation, bool &Digits, bool &filter)
+{
+    char choice;
+    cout << "Оберіть фільтр:\n";
+    cout << "1. Символи латинського алфавіту\n";
+    cout << "2. Символи кирилиці\n";
+    cout << "3. Символи пунктуації\n";
+    cout << "4. Цифри\n";
+    cout << "Уведіть ваш вибір: ";
+    cin >> choice;
+    cin.ignore();
+
+    switch (choice)
+    {
+        case '1':
+            Latin = true;
+            filter = true;
+            break;
+            
+        case '2':
+            Cyrillic = true;
+            filter = true;
+            break;
+                
+        case '3':
+            Punctuation = true;
+            filter = true;
+            break;
+                
+        case '4':
+            Digits = true;
+            filter = true;
+            break;
+            
+        default:
+            cout << "Помилка\n";
+            break;
+        }
+}
+
+// Основная функция
+int main()
+{
+    string text;
+    cout << "Уведіть текст: ";
+    getline(cin, text);
+
+    bool Latin = false;
+    bool Cyrillic = false;
+    bool Punctuation = false;
+    bool Digits = false;
+    bool filter = false;
+
+    chooseFilters(Latin, Cyrillic, Punctuation, Digits, filter);
+   
+    if (filter)
+    {
+        filterText(text, Latin, Cyrillic, Punctuation, Digits);
+        cout << "Відфільтрований текст: " << text << endl;
+    }
+    
+    return 0;
+}
